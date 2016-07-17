6ef066479400c753c6910c7b49deff2c6b8d6457
Sin <Sin>
2016-07-17 09:37:46 +0800

    Changes the some code on FAQ and connect random trip to database

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var db = require('./sqlFAQ.js');
+var db = require('./sql.js');
 
 // Create bot and bind to console
 // Setup Restify Server
@@ -27,55 +27,97 @@ bot.dialog('/', dialog);
 
 // Add intent handlers
 dialog.matches('FAQ', [
-    function (session, args,results) {
+    function (session, args) {
         // Prompt for title
         //show me the trip in peneng price bellow rm80
         var qkey = builder.EntityRecognizer.findAllEntities(args.entities,'QuestionKey');
+		var answer = [];
+		
           console.log(qkey);
-          if(qkey.length>0)
-        {  db.getAnswer(qkey);
-          //setTimeout(function(){},1000);
-          setTimeout(function(){
+        if(qkey.length>0)
+        {  
+			db.getAnswer(qkey);
+          
+			//setTimeout to run function after db.getAnswer finished executed;
+			setTimeout(function(){
+			if (db.result.length>1){
+				
+				/*for(var i=0; i<db.result.length; i++){
+					answer += i+1 +". ";
+					answer += db.result[i]['question'];
+					answer += "\n";
 
-            session.send('Answer: %s',db.answer[0]['answer']);
-            session.endDialog();
+				}
+				endDialog();*/
+				for(var i=0; i<db.result.length; i++){
+					
+					answer.push(db.result[i]['question']);
+					
+				}
+				builder.Prompts.choice(session,'Did you mean: ',answer);
+
+
+			}
+			else if(db.result.length=1){
+
+				session.send('Answer: %s',db.result[0]['answer']);
+				
+			}
 
           },1000);
         }
         else {
-          session.endDialog("Sorry, I didn't understand.");
+          session.send("Sorry, I didn't understand.");
         }
 
-    }
+    },
+	function (session, results) {
+		if (results.response){
+			var keywords = [{entity:results.response.entity}];
+			db.getAnswer(keywords);
+			setTimeout(function(){
+				session.send('Answer: %s',db.result[0]['answer']);
 
+			}	
+			,1000);
+		}
+		session.endDialog();
+	}
 
 ]);
 
+
+//Greeting to the user 
 dialog.matches('Greeting',[
 	function (session) {
         session.send('Hi! Welcome to ChatBot for Departsoon!!!');
 	}
 ]);
 
+//if user need help
 dialog.matches('Help',[
 	function (session) {
         session.send('This bot can let user to search about the travelling package from departsoon and answer users question according to the FAQ. \nUser can also request for random trip package.');
 	}
 ]);
 
+//suggest user a random trip 
 dialog.matches('RandomTrip', [
    function (session) {
+	   db.getAllListing();
+	   
 
-		var random = Math.floor((Math.random() * data.length));
-		var randTrip = data[random];
+		var random = Math.floor((Math.random() * db.result.length));
+		var randTrip = db.result[random];
 		//store entities for random trip
 
-		var trip = 'Trip:\n' + randTrip['trip'] + '\n\nIntroduction:' + randTrip['introduction'] + '\n\nLocation:\n ' + randTrip['location'] + '\n\nDuration:\n' + randTrip['duration'] + '\n\nLanguage:\n' + randTrip['language'] + '\n\nPrice:\n' + randTrip['price'] + '\n\nIncluded:\n' + randTrip['included'] + '\n\nPackage:\n' + randTrip['pack'] + '\n\nReminder:\n' + randTrip['reminder'] + '\n\nLink:\n' + randTrip['link'];
+		var trip = 'Trip:\n' + randTrip['header'] + '\n\nIntroduction:'+ randTrip['experience'] + '\n\nExperience:' + randTrip['introduction'] + '\n\nLocation:\n ' + randTrip['location'] + '\n\nDuration:\n' + randTrip['duration'] + '\n\nLanguage:\n' + randTrip['language'] + '\n\nPrice:\n' + randTrip['price'] + '\n\nIncluded:\n' + randTrip['included'] +  '\n\nReminder:\n' + randTrip['reminder'] + '\n\nLink:\n' + randTrip['link'];
 		session.send(trip);
 		session.endDialog();
     }
 
 ]);
+
 
 bot.use({
     dialog: function (session) {
@@ -87,58 +129,3 @@ bot.use({
 dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand."));
 
 
-//sample data
-var data = [
-  {
-	  trip: 'Nictar Symbiosis Farm Tour',
-      location: 'Malaysia , Pekan Nenas, Kuala Lumpur',
-      duration: '2 hours',
-      language: 'English, Mandarin, Malay, Cantonese ',
-      price: 'from MYR 14',
-	  introduction: 'At Nictar Symbiosis Farm, you are not just seeing the pineapple farm, you will see how different living creatures grow and live together.',
-	  pack: ['Farm visit + Pineapple juice','Farm visit'],
-	  included: ['Pineapple','Farm tour'],
-      Reminder: ['Minimum 10 people in order to fullfill this trip. In the event if participant is less than 10 people, the tour will be cancelled, a cancellation E-mail notification will be sent 1 days before departure.','In the event of typhoons, storms and other poor weather, we will determine whether we will cancel the tour 0 day before departure at local time 9:00 and notify customer via Email.'],
-      //Experience: 'Nictar Symbiosis Farm is located in Pontian\'s Parit Sikom. \nIn short, symbiosis means different living organisms grow together at the same location. \nOur farm has a total area of 10 acres. We have grow several species of pineapple, bees, chicken, and some dogs to guard our farm! And best of all, they all coexist in the farm and bring benefits to one another.\nCome here to learn the concept of symbiosis and knowledge of pineapple. You will love eating pineapple again after this tour.',
-	  link: 'https://www.departsoon.com/en/tour/16/nictar-symbiosis-farm-tour'
-  },
-  {
-	  trip: 'Glow in dark 3D art',
-      location: 'Malaysia , Johor Bahru',
-      duration: '2 hours',
-      language: 'English, Mandarin, Malay, Cantonese',
-	  price: 'from MYR 25',
-      introduction: 'Ever seen 3D art glowing in the dark? At Dark Art 3D Studio you will get to see what we called as "2-in-1 3D art".',
-	  pack: ['-'],
-	  included: ['Entrance'],
-      Reminder: '-',
-      //Experience: 'Dark Art 3D Studio is not your usual 3D art gallery.\n At Dart Art 3D Studio, every art has 2 presentations. \nThe very same art will look differently in different lighting condition.\nVisitors can take a photo with the 3D poster under normal fluorescent lighting. When the light turned off, the art will be glowing in the dark and different layer will appear. \nCome and take some creative photo with your wildest imagination.',
-	  link: 'https://www.departsoon.com/en/tour/14/glow-in-dark-3d-art'
-  },
-  {
-	  trip: 'Container Gardenstay',
-      location: 'Malaysia , Johor Bahru',
-      duration: '-',
-      language: 'English, Mandarin, Malay, Cantonese',
-	  price: 'from MYR 180',
-      introduction: 'Whether you are instagramer, foodie, or just hopping around, Container Gardenstay is an unique staying experience for all kind of people.',
-	  pack: ['-'],
-	  included: 'Lunch/Dinner(Pandan Chicken, Tom yum, Sambal Kangkong )',
-      Reminder: '-',
-      //Experience: 'Located at Taman Desa Tebrau, Container Gardenstay in the compound of the famous Thai Food Restaurant - Bangkok Village. \nContainer Gardenstay is built using several containers. We first saw this new hotel concept in foreign country and then we decided to bring this concept into Johor Bahru. \nEvery container room will have full facilities such as a bathroom, bathroom amenities, television, WiFi internet, big comfy bed, and also window view. \nWe also provide light breakfast for our hotel guest and also dinner for 2 at Bangkok Village restaurant (Only for departsoon\'s buyer).\nBesides, Container Gardenstay is near to many hotspot or business area such as Aeon, Tesco, Johor Jaya.',
-	  link: 'https://www.departsoon.com/en/tour/12/container-gardenstay'
-  },
-  {
-	  trip: 'World Class Classical Guitarist Samuel Klemke live in Pontian',
-      location: 'Malaysia , Pekan Nenas',
-      duration: '2 hours  ',
-      language: 'Mandarin ',
-	  price: 'from MYR 30',
-      introduction: 'World Class Classical Guitarist Samuel Klemke live in Pontian, Johor',
-	  pack: ['-'],
-	  included: 'concert',
-      Reminder: '-',
-      //Experience: 'World class classical guitarist from Germany, for the first time, visit Johor to perform a tour concerts, and he is coming to Pontian, Johor to perform the aforesaid concert on 17th of June, 2015 venue as below:\n\nVenue: 笨珍培群独立中学李织霞讲堂 \nTime: 8pm ',
-	  link: 'https://www.departsoon.com/en/tour/24/world-class-classical-guitarist-samuel-klemke-live-in-pontian'
-  }
-];
